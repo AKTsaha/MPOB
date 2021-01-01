338fdab306f5d8e57bee8ede05797cbeb565a03d
Ankit saha <ankitsahas18@gmail.com>
2021-01-01 19:38:29 +0530

    first

---
+++ js/home.js
@@ -0,0 +1,5 @@
+document.getElementsByClassName("search-box").style.AnimationPlayState = "running";
+document.getElementsByClassName("search-icon").click = function(){
+    alert("working click");
+    document.getElementsByClassName("search-box").style.AnimationPlayState = "running";
+};

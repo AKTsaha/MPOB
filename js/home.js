cd7d6c2f4fbf4ee74f58636d00be3509b37c38d9
Ankit saha <ankitsahas18@gmail.com>
2021-01-05 11:40:06 +0530

    #2

--- js/home.js
+++ js/home.js
@@ -1,5 +1,19 @@
-document.getElementsByClassName("search-box").style.AnimationPlayState = "running";
-document.getElementsByClassName("search-icon").click = function(){
-    alert("working click");
-    document.getElementsByClassName("search-box").style.AnimationPlayState = "running";
-};
+$(document).ready(function() {
+
+
+});
+setTimeout(alert("asdfbasd"), 3000);
+function start(){
+  $(".side-bar").css("filter","blur(10px)");
+  $(".tag-box").css("filter","blur(10px)");
+  $(".option").css("filter","blur(10px)");
+}
+function end(){
+  $(".side-bar").css("filter","blur(0px)");
+  $(".tag-box").css("filter","blur(0px)");
+  $(".option").css("filter","blur(0px)");
+}
+$(".search-icon").click(function() {
+    $(".search-box").css({"animation-play-state": "running"});
+    //$(".search-box").delay(5000).css("animation-play-state", "paused");
+});
